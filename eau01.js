651d46fff710557ccaf84a44b3dd863d374e866a
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-01 18:32:53 +0200

    🌊 Second program done. Same vibe than the first one

---
+++ eau01.js
@@ -0,0 +1,24 @@
+// Combinaison de 2 nombres
+let combinations = [];
+
+// Functions
+function combinationOfTwoNumbers(array) {
+  let combinationString;
+  for (let i = 0; i < 99; i++) {
+    for (let j = 1; j < 100; j++) {
+        if (i < j) {
+          let firstNumber = i < 10 ? "0" + i : i;
+          let secondNumber = j < 10 ? "0" + j : j;
+          array.push(`${firstNumber} ${secondNumber}`);
+        }
+    }
+  }
+}
+
+// Problem solving
+combinationOfTwoNumbers(combinations);
+
+// Results
+for (let combination of combinations) {
+  process.stdout.write(` ${combination},`);
+}
