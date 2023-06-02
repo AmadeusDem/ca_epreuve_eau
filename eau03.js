9e32c92dacd23d140077faf64baf6714ddb3fe35
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-02 15:34:06 +0200

    🌊 Fist recursive function used

---
+++ eau03.js
@@ -0,0 +1,37 @@
+// Suite de Fibonacci
+const args = process.argv;
+
+// Functions
+function printError() {
+  console.log(-1);
+}
+
+// Very basic fibonnaci sequence with recursive (not very optimized for big numbers though)
+function fibonnaciSequence(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fibonnaciSequence(n - 1) + fibonnaciSequence(n - 2);
+  }
+}
+
+// Handling errors
+if (args.length !== 3) {
+  printError();
+  return;
+}
+if (isNaN(args[2])) {
+  printError();
+  return;
+}
+
+// Parsing
+const userInput = args[2];
+
+// Solving problem
+const result = fibonnaciSequence(userInput);
+
+// Printing result
+console.log(result);
