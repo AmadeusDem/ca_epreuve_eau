19740239e029be1dd3cb5d395f94b74adeab8c3f
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-03 19:33:08 +0200

    🌊 Another one is done.

---
+++ eau06.js
@@ -0,0 +1,37 @@
+// Majuscule sur deux
+const args = process.argv;
+const userInput = args[2];
+// Functions
+function printError() {
+  console.error("Error.");
+}
+
+function oneInTwoUpperCase(userString) {
+  let upper = true;
+  let returnString = "";
+  for (let char of userString) {
+    const isALetter = char.match(/[a-zA-Z]/gm) !== null;
+    if (isALetter) {
+      if(upper) char = char.toUpperCase();
+      upper = !upper;
+    }
+    returnString += char;
+  }
+  return returnString;
+}
+
+// Handling errors
+if (args.length !== 3) {
+  printError();
+  return;
+}
+if (!isNaN(userInput)) {
+  printError();
+  return;
+}
+
+// Solving problems
+const result = oneInTwoUpperCase(userInput);
+
+// Printing results
+console.log(result);
