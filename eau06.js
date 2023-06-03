ca3f23a55be4e173594635ec54e60136bfac7fff
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-03 23:22:30 +0200

    🌊 Adding the case where a letter is in UPPER and needs to be lower

--- eau06.js
+++ eau06.js
@@ -8,12 +8,15 @@ function printError() {
 
 function oneInTwoUpperCase(userString) {
   let upper = true;
+  let lower = false;
   let returnString = "";
   for (let char of userString) {
     const isALetter = char.match(/[a-zA-Z]/gm) !== null;
     if (isALetter) {
       if(upper) char = char.toUpperCase();
+      if(lower) char = char.toLowerCase();
       upper = !upper;
+      lower = !lower;
     }
     returnString += char;
   }
