df5c75ea1e3c73cf9bf5735f33744f11c9032b02
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-06 14:17:32 +0200

    🌊 Removed an useless variable

--- eau00.js
+++ eau00.js
@@ -4,7 +4,6 @@ let combinations = [];
 
 // Functions I declare
 function combinationOfThreeDigits(array) {
-  let combinationString;
   for (let i = 0; i < 8; i++) {
     for (let j = 1; j < 9; j++) {
       for (let k = 2; k < 10; k++) {
